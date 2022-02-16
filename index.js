a0b3403f820aa48a5eacf1d079ccb1af23701a0b
Joshua <yoonj200@gmail.com>
2022-02-16 13:24:56 -0600

    commit tree 5988442ea44e78a71c94cd04ea6e56a368155ee4

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  // start at index 0, iterate while i < half word length, increment by 1 each time
+  for (let i = 0; i < word.length / 2; i++) {
+    const j = word.length - 1 - i;
+    const startChar = word[i];
+    const endChar = word[j];
+    //if any letters don't match, return false
+    if (startChar !== endChar) return false;
+  }
+  return true;
 }
 
-/* 
-  Add your pseudocode here
+/* PSEUDOCODE
+  iterate from beginning to middle
+  race car 7 / 2 == 3.5
+  aaaaaa 6 / 2 == 3
+    check each letter to corresponding letter from end
+      if any letter doesn't match, return false
+  return true
 */
 
 /*
   Add written explanation of your solution here
+  first letter is same as last letter, second letter is same as second to last letter, etc.
+  until we reach the middle, return true
+  
+  iterate from beginning to middle
+    check each letter to corresponding letter from end
+      if any letters don't match, return false
 */
 
 // You can run `node index.js` to view these console logs
